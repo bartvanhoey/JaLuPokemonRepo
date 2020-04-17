226466bd62c1c941bbf822ec2d531bab888b5d87
altandenter <bartvanhoey@gmail.com>
2020-04-17 14:14:17 +0200

    end-part-6

---
+++ JaLuPokemon.Models/Pokemon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JaLuPokemon.Models
+{
+    public class Pokemon
+    {
+        public int PokemondId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+        public int Total { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpeedAttack{ get; set; }
+        public int SpeedDefense { get; set; }
+        public int Speed { get; set; }
+        public int Generation { get; set; }
+        public bool Legendary { get; set; }
+
+
+
+
+
+    }
+}
+
