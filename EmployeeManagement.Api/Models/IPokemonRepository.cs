a38b4eaef110c7574bbacbf6961a87f678ac1342
altandenter <bartvanhoey@gmail.com>
2020-04-23 13:56:43 +0200

    end-part-13

---
+++ EmployeeManagement.Api/Models/IPokemonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JaLuPokemon.Models;
+
+namespace JaLuPokemon.Api.Models
+{
+    public interface IPokemonRepository
+    {
+        Task<IEnumerable<Pokemon>> GetPokemons();
+        Task<Pokemon> GetPokemon(int pokemonId);
+        Task<Pokemon> AddPokemon(Pokemon pokemon);
+        Task<Pokemon> UpdatePokemon(Pokemon pokemon);
+        void DeletePokemon(int pokemonId);
+    }
+}
---
+++ EmployeeManagement.Api/Models/PokemonRepository.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JaLuPokemon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JaLuPokemon.Api.Models
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PokemonRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Pokemon>> GetPokemons()
+        {
+            return await _dbContext.Pokemons.ToListAsync();
+        }
+
+        public async Task<Pokemon> GetPokemon(int pokemonId)
+        {
+            return await _dbContext.Pokemons
+                .FirstOrDefaultAsync(e => e.PokemonId == pokemonId);
+        }
+
+        public async Task<Pokemon> AddPokemon(Pokemon pokemon)
+        {
+            var result = await _dbContext.Pokemons.AddAsync(pokemon);
+            await _dbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Pokemon> UpdatePokemon(Pokemon pokemon)
+        {
+            var result = await _dbContext.Pokemons
+                .FirstOrDefaultAsync(e => e.PokemonId == pokemon.PokemonId);
+
+            if (result != null)
+            {
+                result.Name = pokemon.Name;
+                result.PhotoPath = pokemon.PhotoPath;
+                result.TypeOneId = pokemon.TypeOneId;
+                result.TypeTwoId = pokemon.TypeTwoId;
+                result.Legendary = pokemon.Legendary;
+                result.HP = pokemon.HP;
+                result.Defense = pokemon.Defense;
+                result.Attack = pokemon.Attack;
+                result.SpeedAttack = pokemon.SpeedAttack;
+                result.SpeedDefense = pokemon.SpeedDefense;
+                result.PokemonNumber = pokemon.PokemonNumber;
+                result.Generation = pokemon.Generation;
+
+                await _dbContext.SaveChangesAsync();
+
+                return result;
+            }
+
+            return null;
+        }
+
+        public async void DeletePokemon(int pokemonId)
+        {
+            var result = await _dbContext.Pokemons
+                .FirstOrDefaultAsync(e => e.PokemonId == pokemonId);
+            if (result != null)
+            {
+                _dbContext.Pokemons.Remove(result);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
