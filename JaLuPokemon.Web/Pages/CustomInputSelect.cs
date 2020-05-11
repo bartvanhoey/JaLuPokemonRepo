e0deaedcf11762237b944cf2cc5515342d59a0be
altandenter <bartvanhoey@gmail.com>
2020-05-11 21:22:08 +0200

    end-part-31

---
+++ JaLuPokemon.Web/Pages/CustomInputSelect.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace JaLuPokemon.Web.Pages
+{
+    public class CustomInputSelect<TValue> : InputSelect<TValue>
+    {
+        protected override bool TryParseValueFromString(string value, out TValue result, out string validationErrorMessage)
+        {
+
+            if (typeof(TValue) == typeof(int))
+            {
+                if (int.TryParse(value, out var resultInt))
+                {
+                    result = (TValue)(object)resultInt;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage = $"The selected value {value} is not a valid number.";
+                    return false;
+                }
+            }
+            else if (typeof(TValue) == typeof(int?))
+            {
+                if (int.TryParse(value, out int resultInt))
+                {
+                    result = (TValue)(object)resultInt;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else if (value == null)
+                {
+                    result = (TValue)(object)value;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage = $"The selected value {value} is not a valid number.";
+                    return false;
+                }
+            }
+
+            return base.TryParseValueFromString(value, out result, out validationErrorMessage);
+        }
+    }
+}
