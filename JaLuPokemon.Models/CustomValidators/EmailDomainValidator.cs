39576bca52622e798d0ebcec1778fb9b61b5c1eb
altandenter <bartvanhoey@gmail.com>
2020-05-12 14:03:47 +0200

    end-part-37

--- JaLuPokemon.Models/CustomValidators/EmailDomainValidator.cs
+++ JaLuPokemon.Models/CustomValidators/EmailDomainValidator.cs
@@ -6,21 +6,22 @@ namespace JaLuPokemon.Models.CustomValidators
     {
         public string AllowedDomain { get; set; }
         public new string ErrorMessage { get; set; }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext){
-                string[] strings = value.ToString().Split('@');
-                if (strings[1].ToUpper() == AllowedDomain.ToUpper())
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] strings = value.ToString().Split('@');
+            if (strings.Length > 1 && strings[1].ToUpper() == AllowedDomain.ToUpper())
+            {
+                return null;
+            }
+            else
+            {
+                var errorMessage = $"Domain Name must be {AllowedDomain}";
+                if (!string.IsNullOrEmpty(ErrorMessage))
                 {
-                    return null;
+                    errorMessage = ErrorMessage;
                 }
-                else
-                {
-                    var errorMessage = $"Domain Name must be {AllowedDomain}";
-                    if (!string.IsNullOrEmpty(ErrorMessage))
-                    {
-                        errorMessage = ErrorMessage;
-                    }
-                    return new ValidationResult(errorMessage, new []{ validationContext.MemberName});
-                }
+                return new ValidationResult(errorMessage, new[] { validationContext.MemberName });
+            }
         }
     }
 }
